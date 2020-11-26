9d2ec7ed9b15c95abf24c4bdf708b45847166d91
Maja <maja669e@stud.kea.dk>
2020-11-26 13:52:46 +0100

    tried to make login

--- eksamen_projektmanager/src/main/java/demo/controllers/UserController.java
+++ eksamen_projektmanager/src/main/java/demo/controllers/UserController.java
@@ -3,9 +3,11 @@ package demo.controllers;
 import demo.data.DataFacadeImpl;
 import demo.model.Project;
 import demo.model.ProjectManagerException;
+import demo.model.User;
 import demo.model.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
@@ -19,15 +21,24 @@ public class UserController {
     //use case controller (GRASP Controller) - injects concrete facade instance into controller
     private UserService userService = new UserService(new DataFacadeImpl());
 
+    @ExceptionHandler(ProjectManagerException.class)
     @GetMapping("/")
-    public String login() {
+    public String login(Model model, Exception exception) {
+        model.addAttribute("message", exception.getMessage());
         return "index";
     }
 
-    @GetMapping("/opret_projekt")
-    public String createProject() {
-        return "opret_projekt";
+    @PostMapping("/login")
+    public String loginUser(WebRequest request) throws ProjectManagerException {
+        //Retrieve values from HTML form via WebRequest
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        User user = userService.Login(userName, password);
+        setSessionInfo(request, user);
+        System.out.println(user + userName);
+        return "redirect:/opret_projekt";
     }
+
 
     @PostMapping("addProject")
     public String addProject(WebRequest request) throws ProjectManagerException {
@@ -41,5 +52,9 @@ public class UserController {
 
     private void setSessionProject(WebRequest request, Project project) {
         request.setAttribute("project", project, WebRequest.SCOPE_SESSION);
+    }
+
+    private void setSessionInfo(WebRequest request, User user) {
+        request.setAttribute("user", user, WebRequest.SCOPE_SESSION);
     }
 }
